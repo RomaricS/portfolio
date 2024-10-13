4513fd8b27c71f580b0d081e4d5b77d843f56942
Romaric SEGLA <romaricboss@gmail.com>
2024-10-13 03:17:13 +0200

    Escaped apostrophe

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -71,7 +71,7 @@ export const HeroSection = () => {
                     </button>
                     <button className='inline-flex items-center gap-2 border border-white bg-white text-gray-900 px-6 h-12 rounded-xl'>
                         <span>👋</span>
-                        <span className='font-semibold'>Let's Connect</span>
+                        <span className='font-semibold'>Let&apos;s Connect</span>
                     </button>
                 </div>
             </div>
