930bd27a9d5e284b3d0e0cb9716b86574a43c9d3
Romaric SEGLA <romaricboss@gmail.com>
2025-10-18 22:44:20 +0200

    Improved accessibility

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import { HeroSection } from '@/sections/Hero';
-import { Header } from '../sections/Header';
+import { Header } from '@/sections/Header';
 import { ProjectsSection } from '@/sections/Projects';
 import { TapeSection } from '@/sections/Tape';
 import { TestimonialsSection } from '@/sections/Testimonials';
@@ -9,15 +9,17 @@ import { AboutSection } from '@/sections/About';
 
 export default function Home() {
   return (
-    <div>
+    <>
       <Header />
-      <HeroSection />
-      <ProjectsSection />
-      <TapeSection />
-      <TestimonialsSection />
-      <AboutSection />
-      <ContactSection />
+      <main>
+        <HeroSection />
+        <ProjectsSection />
+        <TapeSection />
+        <TestimonialsSection />
+        <AboutSection />
+        <ContactSection />
+      </main>
       <Footer />
-    </div>
+    </>
   );
 }
